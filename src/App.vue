5904d71ecb26827b5ee7e9ef88a1e84b4c6fdc0f
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-25 13:21:33 +0100

    vue files and root component

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  section#glowna
+    h1 {{ title }}
+    p {{ greeting() }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'first vue file'
+    }
+  },
+  methods: {
+    greeting () {
+      return 'hello';
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
