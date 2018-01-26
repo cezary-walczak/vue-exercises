42c098e12807ea80a4097279ed2418aff2323184
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-26 10:12:55 +0100

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,23 @@
 <template lang="pug">
   section#glowna
-    app-header(v-bind:title='title' v-on:changeTitle='updateTitle($event)')
-    app-ninjas(v-bind:ninjas='ninjas')
-    app-footer(v-bind:title='title')
-    form-helper
-      h2(slot='title') {{ slotTitle }}
-      p(slot='text')  I am paragraph text for slot
-      hr
-      h1 please fill out the form
-      form
-        #form-header
-          slot(name='form-header')
-        #form-fields
-          slot(name='form-fields')
-        #form-controls
-          slot(name='form-controls')
-        #useful-links
-          ul
-            li
-              a link1
-              a link2
-              a link3
-    
-    keep-alive
-      component(v-bind:is='component')
-    button(v-on:click='component = "form-one"') Show form 1
-    button(v-on:click='component = "form-two"') Show form 2
+    add-blog
     
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Ninjas from './components/Ninjas.vue';
-import Footer from './components/Footer.vue';
-import FormHelper from './components/FormHelper.vue';
-import FormOne from './components/FormOne.vue';
-import FormTwo from './components/FormTwo.vue';
+import AddBlog from './components/AddBlog.vue'
 
 export default {
   components: {
-    'app-header': Header,
-    'app-ninjas': Ninjas,
-    'app-footer': Footer,
-    'form-helper': FormHelper,
-    'form-one': FormOne,
-    'form-two': FormTwo
+    'add-blog': AddBlog
   },
   data () {
     return {
-      component: 'form-two',
-      title: 'ninja app',
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ],
-      slotTitle: 'I am dynamic slot title'
+
     }
   },
   methods: {
-    updateTitle(updatedTitle) {
-      this.title = updatedTitle;
-    }
+
   }
 }
 </script>
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  section#add-blog
+    h2 Add a new blog post
+    form
+      label Blog Title:
+      input(type='text' v-model.lazy='blog.title' required)
+      label Blog Content:
+      textarea(v-model.lazy='blog.content')
+    #preview
+      h3 Preview blog
+      p Blog Title: {{ blog.title }}
+      p Blog Content:
+      p {{ blog.content }}
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      blog: {
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  section#add-blog
+    h2
+    form
+      label,
+      input,
+      textarea
+        display block
+        margin 2vh 2vw
+        width 20vw
+      textarea 
+        height 20vh
+    #preview
+      h3,
+      p
+        display block
+        margin 2vh 2vw
+        width 20vw
+        
+</style>
