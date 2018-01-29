2138cd013c6b5cd7ceafdfda932050c3cac6267a
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-01-29 20:39:23 +0100

    mapping getters and actions

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -8,19 +8,22 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+import {mapActions} from 'vuex';
+
 export default {
   computed: {
     products () {
       return this.$store.state.products;
     },
-    saleProducts () {
-      return this.$store.getters.saleProducts;
-    }
+    ...mapGetters([
+      'saleProducts'
+    ])
   },
   methods: {
-    reducePrice (amount) {
-      this.$store.dispatch('reducePrice', amount);
-    }
+    ...mapActions([
+      'reducePrice'
+    ])
   }
 }
 </script>
